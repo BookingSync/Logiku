2b255aadd876930520abd064f385ba348545ba75
thilonel <naitodai@gmail.com>
2016-11-19 01:48:03 +0100

    creat key value formatter

--- lib/logiku/formatters/key_value.rb
+++ lib/logiku/formatters/key_value.rb
@@ -1,5 +1,24 @@
 module Logiku::Formatters
   class KeyValue
+    def call(data)
+      data.map { |k, v| format k, v }.join " "
+    end
 
+    private
+
+    def format(k, v)
+      %Q{"#{k}"="#{format_value v}"}
+    end
+
+    def format_value(v)
+      case v
+      when String
+        v.gsub('"') { %q|\"| }
+      when Hash, Array
+        v.inspect.gsub('"') { %q|\"| }
+      else
+        v.inspect
+      end
+    end
   end
 end
--- spec/formatters/key_value_spec.rb
+++ spec/formatters/key_value_spec.rb
@@ -2,8 +2,26 @@ require "spec_helper"
 
 module Logiku::Formatters
   RSpec.describe KeyValue do
-    it "can be inited" do
-      KeyValue.new
+    let(:formatter) { KeyValue.new }
+
+    describe "#call" do
+      it "returns a key value formatted string from the given hash" do
+        expect(formatter.call({ string: %q{ein ' "}, symbol: :symbol }))
+          .to eq %q{"string"="ein ' \\"" "symbol"=":symbol"}
+
+        expect(formatter.call({ integer: 2, float: 3.0 }))
+          .to eq %q{"integer"="2" "float"="3.0"}
+
+        object = Object.new
+        expect(formatter.call(object: object))
+          .to eq %Q{"object"="#{object.inspect}"}
+
+        expect(formatter.call(hash: { "stringkey" => 1, hash_key: "foo" }))
+          .to eq %Q("hash"="{\\"stringkey\\"=>1, :hash_key=>\\"foo\\"}")
+
+        expect(formatter.call(array: ["a", "b", "c"]))
+          .to eq %Q("array"="[\\"a\\", \\"b\\", \\"c\\"]")
+      end
     end
   end
 end
